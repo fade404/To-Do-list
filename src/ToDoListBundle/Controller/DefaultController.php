fd0ed754b4a5e97bfbb4128fc2a97c8f63ec9dbf
fade404 <fade@poczta.onet.pl>
2016-08-12 00:15:03 +0200

    Added showAll and add actions

--- src/ToDoListBundle/Controller/DefaultController.php
+++ src/ToDoListBundle/Controller/DefaultController.php
@@ -5,15 +5,56 @@ namespace ToDoListBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use ToDoListBundle\Entity\Task;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
-     * @Route("/hello/{name}")
+     * @Route("/showAll")
      * @Template()
      */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
+    public function indexAction() {
+        $taskRepo = $this->getDoctrine()->getRepository('ToDoListBundle:Task');
+        $tasks = $taskRepo->findAll();
+
+        return [
+            'tasks' => $tasks
+        ];
     }
+
+    /**
+     * @Route("/add")
+     * @Template()
+     */
+    public function addAction(Request $req) {
+
+        $taskRepo = $this->getDoctrine()->getRepository('ToDoListBundle:Task');
+        $task = new Task();
+        
+        $form = $this->createFormBuilder($task)
+                ->add('title', 'text', ['label' => 'Nazwa zadania'])
+                ->add('description', 'text', ['label' => 'Treść'])
+                ->add('date', 'date', ['label' => 'Termin'])
+                ->add('save', 'submit', ['label' => 'Dodaj'])
+                ->getForm();
+        
+        $form->handleRequest($req);
+        
+        if ($form->isValid()) {
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+            
+            return [
+                'success'   => TRUE
+            ];
+        }
+        
+        return [
+            'form'  => $form->createView()
+        ];
+    }
+
 }
