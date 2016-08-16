ecc40f4d0e8fbee337de73a65f10d86a8af14da0
fade404 <fade@poczta.onet.pl>
2016-08-16 22:08:18 +0200

    Added deleteAction

--- src/ToDoListBundle/Controller/DefaultController.php
+++ src/ToDoListBundle/Controller/DefaultController.php
@@ -11,55 +11,78 @@ use ToDoListBundle\Entity\Task;
 class DefaultController extends Controller {
 
     /**
-     * @Route("/showAll")
+     * @Route("/", name="showAll")
      * @Template()
      */
     public function indexAction() {
         $taskRepo = $this->getDoctrine()->getRepository('ToDoListBundle:Task');
         $tasks = $taskRepo->findAll();
 
-        if(!$tasks) {
+        if (!$tasks) {
             return [
                 'error' => 'Brak zadań'
             ];
         }
-        
+
         return [
             'tasks' => $tasks
         ];
     }
 
     /**
-     * @Route("/add")
+     * @Route("/add", name="add")
      * @Template()
      */
     public function addAction(Request $req) {
 
         $taskRepo = $this->getDoctrine()->getRepository('ToDoListBundle:Task');
         $task = new Task();
-        
+
         $form = $this->createFormBuilder($task)
                 ->add('title', 'text', ['label' => 'Nazwa zadania'])
                 ->add('description', 'text', ['label' => 'Treść'])
                 ->add('date', 'date', ['label' => 'Termin'])
                 ->add('save', 'submit', ['label' => 'Dodaj'])
                 ->getForm();
-        
+
         $form->handleRequest($req);
-        
+
         if ($form->isValid()) {
-            
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($task);
             $em->flush();
-            
+
             return [
-                'success'   => TRUE
+                'success' => TRUE
             ];
         }
-        
+
         return [
-            'form'  => $form->createView()
+            'form' => $form->createView()
+        ];
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @Template()
+     */
+    public function deleteAction($id) {
+        $taskRepo = $this->getDoctrine()->getRepository('ToDoListBundle:Task');
+        $task = $taskRepo->find($id);
+
+        if (!$task) {
+            return [
+                'error' => 'Brak takiego zadania!'
+            ];
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($task);
+        $em->flush();
+
+        return [
+            'success' => TRUE
         ];
     }
 
